25810649318b5df8f6df8ad86988e17ede0dbb62
SAHIL0455 <sahilpawar1004@gmail.com>
2022-11-15 15:34:07 +0530

    ADDED: add validation to all fields

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollAppDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollAppDTO.java
@@ -1,17 +1,35 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.aspectj.bridge.Message;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeePayrollAppDTO {
+    @NotEmpty(message = "Employee First name is mandatory")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z]{2,}$", message = "Employee First Name Invalid")
     private String firstName;
+
+    @NotEmpty(message = "Employee First name is mandatory")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z]{2,}$", message = "Employee First Name Invalid")
     private String lastName;
+
     private String profilePic;
+
+    @NotEmpty(message = "Notes is mandatory")
     private String note;
+
+    @Min(15000)
     private long salary;
+
+    @NotEmpty(message = "Start Date is mandatory")
     private String startDate;
 }
